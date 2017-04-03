896c61c82e4a5f02010fea13fdfb7493e4605ad7
aso4 <albertyso@gmail.com>
2017-04-03 11:35:11 -0400

    in keyword equivalent in string array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,25 +4,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int value = 7;
 
-        int[] values;
+        String[] fruits = {"apple" , "banana", "pear", "kiwi"};
 
-        values = new int[3]; // allocates memory to hold 3 integers
-        values[0] = 10;
-
-        System.out.println(values[0]);
-        System.out.println(values[1]);
-        System.out.println(values[2]);
-
-        for(int i=0; i<values.length; i++) {
-            System.out.println(values[i]);
+        for(String fruit: fruits) { // in equivalent
+            System.out.println(fruit);
         }
 
-        int[] numbers = {5, 6, 7}; // specify the values at initialization
-
-        for(int i=0; i<numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
+        // null == nothing
     }
 }
