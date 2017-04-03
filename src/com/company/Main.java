fa1a856d6c86b13449a201668a4f054cc6e00ba4
aso4 <albertyso@gmail.com>
2017-04-03 07:19:52 -0400

    declare int

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+        int myNumber;
+
+        myNumber = 88;
+
+        System.out.println(myNumber);
     }
 }
