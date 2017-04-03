2e99b68404edd9ed52767636adb859fa7a1e9e7d
aso4 <albertyso@gmail.com>
2017-04-03 13:41:56 -0400

    add method to class and call method in main

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -3,26 +3,24 @@ class Person {
     String name;
     int age;
 
+    void speak() {
+        System.out.println("Hello, my name is " + name + ", I'm " + age + " years old.");
+    }
 }
 
 public class App {
 
-/*   error message: Exception in thread "main" java.lang.ClassNotFoundException: com.company.Main
-     resolved via editing run/debug configuration and editing "Main class" field to point to App
-     instead of com.company.App:
-     http://stackoverflow.com/questions/41916575/error-class-not-found-in-intellij-ide-on-mac
-     */
     public static void main(String[] args) {
 
         Person person1 = new Person();
         person1.name = "Joe Bloggs";
         person1.age = 37;
+        person1.speak();
 
         Person person2 = new Person();
         person2.name = "Sarah Smith";
         person2.age = 20;
-
-        System.out.println(person1.name);
+        person2.speak();
 
     }
 }
