60f4ee018cfdf9a2ac54c66f3481a93d02e9be0f
aso4 <albertyso@gmail.com>
2017-04-03 07:24:06 -0400

    declare primitive types

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //primitive types
+        int myNumber = 88;
+        short myShort = 847;
+        long myLong = 9797;
 
-        int myNumber;
+        double myDouble = 7.3243;
+        float myFloat = 324.3f; // need the f
 
-        myNumber = 88;
+        char myChar = 'y'; // char type
+
+        boolean myBoolean = true;
+        byte myByte = 127; // 8 bits of data. +-127
 
         System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
     }
 }
